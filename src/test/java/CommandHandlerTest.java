8d884acc7926d6d1a68eb08ce7c444c00e86f296
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-11-10 16:27:15 +0100

    Ajout du gestionnaire de commandes et de ses tests

---
+++ src/test/java/CommandHandlerTest.java
@@ -0,0 +1,33 @@
+import org.example.CommandHandler;
+import org.example.models.Command;
+import org.example.models.items.PS5;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommandHandlerTest {
+    private CommandHandler commandHandler;
+
+    @Before public void before() {
+        commandHandler = new CommandHandler();
+    }
+
+    @Test public void aCommandIsAddedToTheListSuccess() {
+        Command command = new Command();
+
+        Assert.assertEquals(0, commandHandler.getCommands().size());
+
+        commandHandler.addCommand(command);
+        Assert.assertEquals(1, commandHandler.getCommands().size());
+    }
+
+    @Test public void aCommandIsAddedToTheListFail() {
+        Command command = new Command();
+        command.setState(Command.State.IN_PROGRESS);
+
+        Assert.assertEquals(0, commandHandler.getCommands().size());
+
+        commandHandler.addCommand(command);
+        Assert.assertEquals(0, commandHandler.getCommands().size());
+    }
+}
