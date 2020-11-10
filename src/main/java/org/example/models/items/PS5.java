baab352516d8d8a6e2e110fe84c6c1a4f8642ad0
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-11-10 16:26:48 +0100

    Ajout des items commandables

---
+++ src/main/java/org/example/models/items/PS5.java
@@ -0,0 +1,7 @@
+package org.example.models.items;
+
+public class PS5 extends Console {
+    public PS5() {
+        this.setName("PS5");
+    }
+}
---
+++ src/main/java/org/example/models/items/Strap.java
@@ -0,0 +1,7 @@
+package org.example.models.items;
+
+public class Strap extends Goodie {
+    public Strap() {
+        this.setName("Strap");
+    }
+}
